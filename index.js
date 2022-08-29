d0dae51d3b6fc194b23620ae27370f8cb9b16601
joseph <josephmacharia575@gmail.com>
2022-08-29 22:00:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+	let tournmentRecord = record.find(function (tournmentRecord ) {
+		return tournmentRecord .result === "W";
+	});
+	if (typeof tournmentRecord  === "undefined") {
+		return undefined;
+	} else {
+		return tournmentRecord .year;
+	}
+}
